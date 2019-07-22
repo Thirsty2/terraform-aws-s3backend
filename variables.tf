2e9a8febe29ec82b0420e1c19e57f583c7e92e04
Scott Winkler <Scott.Winkler@elliemae.com>
2019-07-21 17:34:33 -0700

    update

--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "namespace" {
 }
 
 variable "assume_role_account_id" {
-  description = "id of AWS account that can assume role"
+  description = "AWS account id that can assume role"
   type        = string
 }
 
 variable "force_destroy_s3" {
-  description = "force destroy s3 bucket?"
+  description = "Force destroy s3 bucket?"
   default     = true
   type        = bool
 }
